fa6eb9a59f156a62d927004c9f96d5587c108938
David Fuentes <fuentes@SCRGP2.(none)>
2012-03-25 14:02:31 -0500

    need to make sure that the bounds do not interfere..

--- driver.m
+++ driver.m
@@ -16,7 +16,7 @@ LowerBound(:,:,1) = 0; % amplitide: 0-10
 LowerBound(:,:,2) = 2 ; % shape : 2-7
 LowerBound(:,:,3) = 1 ; % scale : 1-3
 LowerBound(:,:,4) = 0; % delay: 0-40
-UpperBound(:,:,1) = 10; % amplitide: 0-10
+UpperBound(:,:,1) = 200; % amplitide: 0-10
 UpperBound(:,:,2) = 7 ; % shape : 2-7
 UpperBound(:,:,3) = 3 ; % scale : 1-3
 UpperBound(:,:,4) = 40; % delay: 0-20
@@ -39,10 +39,16 @@ UpperBound(:,:,4) = 40; % delay: 0-20
 %
 %        c = -1; % define parameter first
 %        x = lsqnonlin(@(x) myfun(x,c),[1;1])
-options = optimset('jacobian','off','MaxIter',10000000000,'MaxFunEvals',10000000000);
+options = optimset('jacobian','off','MaxIter',10000000000,'MaxFunEvals',10000000000,'TolFun',1e-8);
 global iterFun 
 iterFun = 0; 
+tic;
+%testExact = ExactData(2:7,2:7,:);
+%deltaExact = testExact + rand(6,6,4);
+%dynProjKernel(testExact,MRData,npixel,roi);
+%[x,resnorm,residual,exitflag,output,lambda] = lsqnonlin( @(x) dynProjKernel(x,MRData,npixel,roi),deltaExact(:),LowerBound(:),UpperBound(:),options);
 [x,resnorm,residual,exitflag,output,lambda] = lsqnonlin( @(x) dynProjKernel(x,MRData,npixel,roi),InitialGuess(:),LowerBound(:),UpperBound(:),options);
+runtime = toc;
 
 switch exitflag
  case 1
@@ -64,6 +70,7 @@ switch exitflag
  otherwise
    disp('unknown ')
 end
+runtime 
 
 %% Generate data based on guess
 %
