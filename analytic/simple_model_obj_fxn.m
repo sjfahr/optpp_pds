13be5907848a7ead630157db16a5b5ef7fac5c6b
Samuel <ignis@SCRGP2.(none)>
2013-11-22 13:37:51 -0600

    Updated super_script.m ; Now I'll try to run it w/ DAKOTA

--- analytic/simple_model_obj_fxn.m
+++ analytic/simple_model_obj_fxn.m
@@ -12,41 +12,24 @@ aaa=strtrim(aaa);
 aaa=regexp(aaa,'\s+','split');
 
 probe_u = str2num(aaa{2}{1});
-g = str2num(aaa{3}{1});
+g_anisotropy = str2num(aaa{3}{1});
 mu_a = str2num(aaa{4}{1});
 mu_s = str2num(aaa{5}{1});
-k = str2num(aaa{6}{1});
-w = str2num(aaa{7}{1});
-x_disp = str2num(aaa{8}{1});
-y_disp = str2num(aaa{9}{1});
-z_disp = str2num(aaa{10}{1});
-x_rot = str2num(aaa{11}{1});
-y_rot = str2num(aaa{12}{1});
-z_rot = str2num(aaa{13}{1});
+k_cond = str2num(aaa{6}{1});
+w_perf = str2num(aaa{7}{1});
+% x_disp = str2num(aaa{8}{1});
+% y_disp = str2num(aaa{9}{1});
+% z_disp = str2num(aaa{10}{1});
+% x_rot = str2num(aaa{11}{1});
+% y_rot = str2num(aaa{12}{1});
+% z_rot = str2num(aaa{13}{1});
 
 robin_co=0; %dummy var
 
 clear aaa;
 
-% % Write every string as a number
-% g=str2num(anfact_healthy);      % Optical anisotropy
-% k=str2num(k_0_healthy);         % Thermal conductivity
-% mu_a=str2num(mu_a_healthy);     % Optical absorption
-% mu_s=str2num(mu_s_healthy);     % Optical scattering
-% probe_u=str2num(probe_init);    % Initial probe temperature
-% %robin_co=str2num(robin_coeff);  % Value of Robin boundary coefficient
-% w=str2num(w_0_healthy);         % Blood perfusion
-% x_disp=str2num(x_displace);     % Horizontal displacement
-% x_rot=str2num(x_rotate);        % Rotation about horizontal axis
-% y_disp=str2num(y_displace);     % Vertical displacement
-% y_rot=str2num(y_rotate);        % Rotation about vertical axis
-% z_disp=str2num(z_displace);     % Depth displacement
-% z_rot=str2num(z_rotate);        % Rotation about depth axis
-
-cd (path22);
-cd '/FUS4/data2/BioTex/BrainNonMDA/processed/Patient0002/000/laser_log'
-load 'power_log.txt';
-[P,~,delta_P] = power_parser(power_log);
+% Load the recorded power
+power_log = load ('time_then_power.csv');
 
 % Define the domain and scaling
 mod_point.x=171;  % x position on image
@@ -54,8 +37,11 @@ mod_point.y=171;
 mod_point.z=1;
 
 % Import the VTK header info
-cd (path22);
-cd '/FUS4/data2/BioTex/BrainNonMDA/processed/Patient0002/000/laser'
+path_append = strrep ( path22, '/FUS4/data2/sjfahrenholtz/gitMATLAB/optpp_pds/workdir/', '');
+path_append = strrep ( path_append, 'opt', '');
+FOV_path = strcat( '/FUS4/data2/BioTex/BrainNonMDA/processed/' , path_append );
+FOV_path = strcat( FOV_path , 'laser' );
+cd ( FOV_path );
 FOV_import = csvimport ( 'FOV.csv' );
 fov = FOV_import ( 2, : );
 fov = cell2mat (fov);
@@ -73,12 +59,12 @@ FOV.y = matrix.y * spacing.y;
 FOV.z = matrix.z * spacing.z;
 
 % For now, x and y scaling must be equal; z = 1
-scaling.x=1;
-scaling.y=1;
-scaling.z=1;
+scaling.x = 1;
+scaling.y = 1;
+scaling.z = 1;
 
 % Build the domain
-[dom,~,mod_pix]=modeled_domain(FOV,matrix,scaling,mod_point);
+[dom,~,~]=modeled_domain(FOV,matrix,scaling,mod_point);
 
 % Define the source; source.n must be greater than 1 to make sense. Odd is
 % better than even
@@ -87,46 +73,83 @@ source.length=0.01;  %~0.033 is when n=5 is visible
 source.laser=linspace((-source.length/2),(source.length/2),source.n);
 
 % Run the Bioheat model with the unique powers
-[tmap_unique]=Bioheat1D(P,dom,source,w,k,g,mu_a,mu_s,probe_u,robin_co);
-
+[tmap_unique]=Bioheat1D( power_log,dom,source,w_perf,k_cond,g_anisotropy,mu_a,mu_s,probe_u,robin_co);
 tmap_unique=tmap_unique+37;
 tmap_unique(:,:,:,1)=37;
+
 % Make the full temperature history with the unique tmaps
-[tmap]=Build_tmap_history(tmap_unique,delta_P);
+% [tmap]=Build_tmap_history(tmap_unique,delta_P);
 
 % Set delta time 'dt' for dose and run it
-dt = 2.5;
-[w,~]=ArrDose_for_1D(tmap,dt);
-w ( w > 4 ) = 4;  %Put a cap on the dose
+% dt = 2.5;
+% [w,~]=ArrDose_for_1D(tmap,dt);
+% w ( w > 4 ) = 4;  %Put a cap on the dose
 
-% Calculate the number of pixels that must be shifted
-pixel_reg.x = round (x_disp / mod_pix.x);
-pixel_reg.y = round (y_disp / mod_pix.y);
-pixel_reg.z = round (z_disp / mod_pix.z);
-
-aa = imresize (w , 1/scaling.x);
-
-cd '/FUS4/data2/BioTex/BrainNonMDA/processed/Patient0002/000/matlab/'
+% Change directory to load Arrhenius dose and MATLAB registration data.
+dose_path = strcat( '/FUS4/data2/BioTex/BrainNonMDA/processed/' , path_append );
+dose_path = strcat( dose_path , 'matlab' );
+cd ( dose_path );
 load 'arrheniusDose.mat'
 MRTI_dose_size=size(arrheniusDose.mean);
+load ( 'VOI.mat' );
 
-% Trim the objective fxn's ROI
-aa_size=size(aa);
+% Resize the tmap_unique model into the same spacing as the MRTI
+aa = imresize (tmap_unique , 1/scaling.x);
+bb = aa(round(matrix.x/2),round(matrix.y/2),1,:);
+[~,dd] = max (bb);
+
+aa_size = size ( aa(:,:,1,dd) );
 size_diff=[(MRTI_dose_size(1)-aa_size(1)) (MRTI_dose_size(2)-aa_size(2))];
 upper_left_mod = zeros((size(aa,1)+size_diff(1)),(size(aa,2)+size_diff(2)));
-upper_left_mod(1:size(aa,1),1:size(aa,2)) = aa;
+upper_left_mod(1:size(aa,1),1:size(aa,2)) = aa(:,:,1,dd);
 
-%Define intervals that will be written
-x_range = [ (pixel_reg.x - floor(aa_size(1)/2)) (pixel_reg.x + floor(aa_size(1)/2))];
-y_range = [ (pixel_reg.y - floor(aa_size(2)/2)) (pixel_reg.y + floor(aa_size(2)/2))];
+% Define the intervals that will be compared
+x_range   = [ (VOI.center_in_pix.y - floor(aa_size(1)/2)) (VOI.center_in_pix.y + floor(aa_size(1)/2))];
+y_range   = [ (VOI.center_in_pix.x - floor(aa_size(2)/2)) (VOI.center_in_pix.x + floor(aa_size(2)/2))];
 
-%matched_mod = zeros(arrheniusDose.mean(1),arrheniusDose.mean(2),);
+roi_x    = [ (VOI.center_in_pix.y - 60) (VOI.center_in_pix.y + 60) ]; %For model 57 deg C isotherms.
+roi_y    = [ (VOI.center_in_pix.x - 60) (VOI.center_in_pix.x + 60) ];
+
+roi_x_MRTI   = [ (VOI.center_in_pix.y - 20) (VOI.center_in_pix.y + 20) ];  %For MRTI dose.
+roi_y_MRTI   = [ (VOI.center_in_pix.x - 20) (VOI.center_in_pix.x + 20) ];
+
+x_range = round ( x_range ); % Round everything after calculating them.
+y_range = round ( y_range );
+roi_x = round ( roi_x );  
+roi_y = round ( roi_y );
+roi_x_MRTI = round ( roi_x_MRTI );
+roi_y_MRTI = round ( roi_y_MRTI );
+
+% Register the model data to the MRTI
 matched_mod = zeros (MRTI_dose_size(1), MRTI_dose_size(2));
 matched_mod ( x_range(1):x_range(2), y_range(1):y_range(2) ,:,:) = upper_left_mod( 1:aa_size(1) , 1:aa_size(2) );
 
-cd (path22);
-[metric] = dose_obj ( matched_mod, arrheniusDose.mean);
+model_Iso = (matched_mod >57 ) ; % 57 deg C isotherm
 
+model_Iso ( 1:roi_x(1), : ) = 0;
+model_Iso ( roi_x(2):end,: )  = 0;
+model_Iso ( :, 1:roi_y(1) ) = 0;
+model_Iso ( :,roi_y(2):end ) = 0;
+
+MRTI_Iso = (arrheniusDose.mean >1 ) ;
+
+MRTI_Iso ( 1:roi_x_MRTI(1), : ) = 0;
+MRTI_Iso ( roi_x_MRTI(2):end, : )  = 0;
+MRTI_Iso ( :, 1:roi_y_MRTI(1) ) = 0;
+MRTI_Iso ( :,roi_y_MRTI(2):end ) = 0;
+
+sum_Iso = model_Iso + MRTI_Iso;
+diff_Iso= model_Iso - MRTI_Iso;
+
+figure(1); imagesc(matched_mod);
+figure(2); imagesc(model_Iso);
+figure(3); imagesc(MRTI_Iso);
+figure(4); imagesc(sum_Iso);
+figure(5); imagesc(diff_Iso);
+
+metric = abs(sum(sum(diff_Iso)));
+
+cd (path22);
 output_param = 'optpp_pds.out.';
 index = num2str(iteration);
 output_filename = strcat( output_param, index);
--- analytic/super_script.m
+++ analytic/super_script.m
@@ -1,10 +1,10 @@
 % This is the superscript that has the paths for all of the patient data.
 
-% Paths.
+% Paths. Be in the folder you want to process. E.g.:
+% '/FUS4/data2/sjfahrenholtz/gitMATLAB/optpp_pds/workdir/Patient0002/000/opt'
 setenv ( 'PATH22' , pwd);
 path22 = getenv ( 'PATH22' );
 
-cd (path22)
 load index.txt
 
 [metric] = simple_model_obj_fxn ( path22, index );
